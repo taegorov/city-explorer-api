fe91cbf65432ec55fdd22b36adff806768c91cb7
taegorov <taegorov@gmail.com>
2021-04-22 14:02:56 -0700

    adds nodemon to env file, error message

--- server.js
+++ server.js
@@ -3,11 +3,13 @@
 const express = require('express');
 require('dotenv').config();
 const cors = require('cors');
-// weather data is an object
-const weather = require('./data/weather.json')
 const app = express();
 const PORT = process.env.PORT || 3002;
 const superagent = require('superagent');
+
+// weather data is an object
+const weather = require('./data/weather.json')
+
 
 app.use(cors());
 
@@ -22,7 +24,7 @@ app.get('/', (request, response) => {
 // // weather placeholder
 // app.get('/weather', (request, response) => {
 //   console.log(request.query);
-  
+
 //   try {
 //     const weatherArray = weather.data.map(day => new Forecast(day));
 //     response.status(200).send(weatherArray);
@@ -44,23 +46,32 @@ app.get('/', (request, response) => {
 // }
 
 
-
 // real-time weather, learned during lecture
 app.get('/weather', getWeatherHandler);
 
+
 async function getWeatherHandler(request, response) {
-  const lat = request.query;
-  const lon = request.query;
-  const key = process.env.WEATHER_API_KEY;
+  try {
 
-  const url = `http://api.weatherbit.io/v2.0/forecast/daily?lat=${lat}&lon=${lon}&key=${key}`;
+    const lat = request.query.lat;
+    const lon = request.query.lon;
+    const key = process.env.WEATHER_API_KEY;
 
-  const weatherResponse = await superagent.get(url);
-  const weatherObject = JSON.parse(weatherResponse.text);
-  const weatherArray = weatherObject.data;
-  const forecasts = weatherArray.map(day => new Forecast(day))
+    const url = `http://api.weatherbit.io/v2.0/forecast/daily?lat=${lat}&lon=${lon}&key=${key}`;
 
-  response.send(forecasts);
+
+    // console.log('url', url);
+
+    const weatherResponse = await superagent.get(url);
+    const weatherObject = JSON.parse(weatherResponse.text);
+    const weatherArray = weatherObject.data;
+    const forecasts = weatherArray.map(day => new Forecast(day))
+
+    response.send(forecasts);
+  } catch(error) {
+    console.log(error);
+    response.send('oops!')
+  }
 }
 
 class Forecast {
@@ -71,10 +82,46 @@ class Forecast {
 }
 
 
-// every other GET request will result in a 404
+// movie database
+app.get('/movies', getMovieHandler);
+
+
+async function getMovieHandler(request, response) {
+  console.log('made it to movies');
+
+  const cityName = request.query.cityName;
+
+  const movieKey = process.env.MOVIE_API_KEY;
+
+  const url = `https://api.themoviedb.org/3/search/movie?query=${cityName}&api_key=${movieKey}`;
+
+  const movieResponse = await superagent.get(url);
+  const movieObject = JSON.parse(movieResponse.text);
+  const movieArray = movieObject.results;
+  const topMovies = movieArray.map(movie => new Movies(movie))
+
+  response.send(topMovies);
+}
+
+class Movies {
+  constructor(movie) {
+    this.title = movie.title;
+    this.popularity = movie.popularity;
+    this.overview = movie.overview;
+    // poster_path is what TMBD decided to name the property
+    this.image = movie.poster_path;
+
+    console.log(movie);
+  }
+}
+
+
+
+// // every other GET request will result in a 404
 app.get('*', (request, response) => {
   response.status(400, 404, 500).send('error: something went wrong');
 })
+
 
 
 // stay alive, listen, keep ears open for any request. rather than running one time only and stopping. "by virtue of listening, we keep it alive"
